5d2328a13a818160e08a1d243a6896fb3fae9024
yzqdev <yzqdev@outlook.com>
2022-05-28 15:38:09 +0000

    Deploying to gh-pages from @ yzqdev/cg-tutor@9bc47f1a1c061ad949c0bfd48e3a5bd32487ed4a 🚀

---
+++ assets/SearchBox.c4ff661e.js
@@ -0,0 +1,55 @@
+import{k as m,q as B,l as S,f as _,v as C,o as h,c as p,x as $,y as I,b as g,z as b,A as w,F as P,B as T,C as L,D as v,E as U}from"./app.662543d1.js";import{_ as x}from"./plugin-vue_export-helper.21dcd24c.js";const Y={},A=[{path:"/",title:"\u9996\u9875",pathLocale:"/",contents:[{header:"\u{1F6E0}\u5F00\u59CB",slug:"\u{1F6E0}\u5F00\u59CB",content:'System.out.println("Hello World!")'}]},{path:"/ae/",title:"ae",pathLocale:"/",contents:[{header:"ae",slug:"ae",content:""}]},{path:"/blender/",title:"blender",pathLocale:"/",contents:[{header:"blender",slug:"blender",content:""},{header:"\u76EE\u5F55",slug:"\u76EE\u5F55",content:`\u6A21\u578B\u4E0B\u8F7D
+\u4F7F\u7528mmd
+mmd\u6CE8\u610F\u4E8B\u9879
+\u63D2\u4EF6\u63A8\u8350
+blender`}]},{path:"/blender/blender-models.html",title:"\u6A21\u578B\u4E0B\u8F7D",pathLocale:"/",contents:[{header:"\u6A21\u578B\u4E0B\u8F7D",slug:"\u6A21\u578B\u4E0B\u8F7D",content:`https://ftp.nluug.nl/pub/graphics/blender/demo
+\u200B
+https://blendswap.com/blend/18811
+\u6750\u8D28\u4E0B\u8F7D https://www.blendermx.com/
+https://www.poliigon.com/
+https://www.blenderkit.com/
+https://polyhaven.com/
+https://quixel.com/
+https://sketchfab.com/
+\u98DE\u8239\u6A21\u578B http://ship.shapewright.com/
+\u200B \u539F\u795E\u6A21\u578B\u4E0B\u8F7D https://www.aplaybox.com/
+http://ys.biligame.com/pjdkx/
+http://ys.biligame.com/gczj/
+https://open3dlab.com/
+https://sketchfab.com/
+https://www.artstation.com/
+\u200B \u4F7F\u7528blender\u548Cmmd
+https://github.com/Manashiku/MMDGenshin
+mmd\u7F51\u7AD9
+http://mmd.xiaolindraw.com/
+\u200B
+\u200B
+\u539F\u795Emmd
+https://www.aplaybox.com/details/model/c9HNzdGMELyJ
+https://www.bilibili.com/video/BV1wT4y1C7k9`}]},{path:"/blender/blender-use-mmd.html",title:"\u4F7F\u7528mmd",pathLocale:"/",contents:[{header:"\u4F7F\u7528mmd",slug:"\u4F7F\u7528mmd",content:`\u5BFC\u5165\u63D2\u4EF6: https://github.com/powroupi/blender_mmd_tools
+\u200B
+\u5BFC\u5165mmd
+https://github.com/GiveMeAllYourCats/cats-blender-plugin`}]},{path:"/blender/mikumiku-dance.html",title:"mmd\u6CE8\u610F\u4E8B\u9879",pathLocale:"/",contents:[{header:"mmd\u6CE8\u610F\u4E8B\u9879",slug:"mmd\u6CE8\u610F\u4E8B\u9879",content:""},{header:"\u5730\u9762\u4E0D\u663E\u793A",slug:"\u5730\u9762\u4E0D\u663E\u793A",content:"\u663E\u793A->\u5173\u95ED\u5730\u9762\u9634\u5F71"},{header:"\u80CC\u666F\u5F02\u5E38",slug:"\u80CC\u666F\u5F02\u5E38",content:"\u80CC\u666F-> \u6A21\u578B\u7ED8\u5236\u987A\u5E8F->\u573A\u666F\u5728\u6700\u4E0A\u9762,\u6A21\u578B\u5728\u6700\u4E0B\u9762"},{header:"\u5BFC\u51FA\u955C\u5934\u6570\u636E",slug:"\u5BFC\u51FA\u955C\u5934\u6570\u636E",content:"\u9009\u62E9\u6240\u6709\u955C\u5934\u5E27-> \u6587\u4EF6=>\u4FDD\u5B58\u52A8\u4F5C\u6570\u636E(\u5176\u4ED6\u7684\u7C7B\u4F3C)"}]},{path:"/blender/plugins.html",title:"\u63D2\u4EF6\u63A8\u8350",pathLocale:"/",contents:[{header:"\u63D2\u4EF6\u63A8\u8350",slug:"\u63D2\u4EF6\u63A8\u8350",content:""},{header:"https://github.com/agmmnn/awesome-blender",slug:"https-github-com-agmmnn-awesome-blender",content:""},{header:"blender\u63D2\u4EF6\u76EE\u5F55",slug:"blender\u63D2\u4EF6\u76EE\u5F55",content:`https://docs.blender.org/manual/zh-hans/dev/advanced/blender_directory_layout.html
+For default installation location addons is usually found in; C:\\Program Files\\Blender Foundation\\blender[version]\\addons
+#steam
+D:\\SteamLibrary\\steamapps\\common\\Blender\\3.0\\scripts\\addons For the secondary "User" location, addons is usually found in; C:\\Users[profile]\\AppData\\Roaming\\Blender Foundation... Design note: the secondary location is typically used as a 'cache', the contents of which are copied back and forth between the temporary storage location and Blenders installation folder. This depends on the version of Blender used rather than the Operating System.`},{header:"bagapie",slug:"bagapie",content:`https://www.f12studio.fr/bagapie
+https://github.com/agmmnn/awesome-blender`},{header:"Poliigon Material Converter Addon for Blender",slug:"poliigon-material-converter-addon-for-blender",content:`https://help.poliigon.com/en/articles/2540839-poliigon-material-converter-addon-for-blender
+\u4E07\u7269\u6709\u7075
+https://shimo.im/docs/qjXWwYkGKccgXVgd/read`},{header:"Node Wrangler\uFF08\u514D\u8D39\uFF0CBlender\u81EA\u5E26\uFF09",slug:"node-wrangler-\u514D\u8D39-blender\u81EA\u5E26",content:""},{header:"MACHIN3tools",slug:"machin3tools",content:""},{header:"Screencast-Keys",slug:"screencast-keys",content:`\u663E\u793A\u60A8\u7684\u6309\u952E
+https://github.com/nutti/Screencast-Keys`},{header:"drop it",slug:"drop-it",content:"https://github.com/maxivz/interactivetoolsblender"},{header:"super io",slug:"super-io",content:"https://github.com/atticus-lv/super_io"},{header:"edge flow",slug:"edge-flow",content:"https://github.com/BenjaminSauder/EdgeFlow"},{header:"blenderkit",slug:"blenderkit",content:"https://www.blenderkit.com/"}]},{path:"/cg-tutor/",title:"cg-tutor",pathLocale:"/",contents:[{header:"cg-tutor",slug:"cg-tutor",content:""},{header:"blender",slug:"blender",content:`\u6A21\u578B\u4E0B\u8F7D
+\u4F7F\u7528mmd
+mmd\u6CE8\u610F\u4E8B\u9879
+\u63D2\u4EF6\u63A8\u8350
+blender`},{header:"maya-tutor",slug:"maya-tutor",content:`\u5B89\u88C5maya\u8F6F\u4EF6
+maya\u6559\u7A0B`},{header:"cg-tutor",slug:"cg-tutor-1",content:`\u5F00\u6E90\u8F6F\u4EF6
+cg\u6559\u7A0B
+\u4E00\u4E9B\u7F51\u7AD9`}]},{path:"/cg-tutor/opensource.html",title:"\u5F00\u6E90\u8F6F\u4EF6",pathLocale:"/",contents:[{header:"\u5F00\u6E90\u8F6F\u4EF6",slug:"\u5F00\u6E90\u8F6F\u4EF6",content:`blender
+krita
+inkscape
+gimp
+shotcut
+mineimator`}]},{path:"/cg-tutor/sites.html",title:"\u4E00\u4E9B\u7F51\u7AD9",pathLocale:"/",contents:[{header:"\u4E00\u4E9B\u7F51\u7AD9",slug:"\u4E00\u4E9B\u7F51\u7AD9",content:"https://blendswap.com/"}]},{path:"/maya/",title:"maya\u6559\u7A0B",pathLocale:"/",contents:[{header:"maya\u6559\u7A0B",slug:"maya\u6559\u7A0B",content:""}]},{path:"/maya/install.html",title:"\u5B89\u88C5maya\u8F6F\u4EF6",pathLocale:"/",contents:[{header:"\u5B89\u88C5maya\u8F6F\u4EF6",slug:"\u5B89\u88C5maya\u8F6F\u4EF6",content:`\u89C1\u9F8B\u9F7F\u4E00\u53F7
+\u94FE\u63A5
+\u8BBE\u7F6E\u8BED\u8A00\u5207\u6362:
+\u5728\u73AF\u5883\u53D8\u91CF\u91CC\u9762\u65B0\u5EFA MAYA_UI_LANGUAGE \u5E76\u8BBE\u7F6E\u4E3Azh_CN\u6216\u8005en_US\u5207\u6362\u4E2D\u82F1\u6587`}]},{path:"/ps/",title:"ps\u6559\u7A0B",pathLocale:"/",contents:[{header:"ps\u6559\u7A0B",slug:"ps\u6559\u7A0B",content:""}]},{path:"/404.html",title:"",pathLocale:"/",contents:[]}],D="update-vuepress2-plugin-full-text-search-search-index";var M=m(A),E=B(()=>{const e=new Map;for(const t of M.value)e.set(t.path,t);return e});Y.webpackHot&&(__VUE_HMR_RUNTIME__[D]=e=>{M.value=e});function F(e){const t=m([]);let a=null;return S(e,()=>{a&&clearTimeout(a),a=setTimeout(o,100)}),t;function o(){const c=e.value.toLowerCase().trim();if(!c){t.value=[];return}const l=new Map,n=new Set;for(const s of M.value)for(const i of N(s,c)){n.add(i.parentPageTitle);let r=l.get(i.parentPageTitle);r||(r=[],l.set(i.parentPageTitle,r)),r.push(i)}const u=[...n].sort((s,i)=>{const r=l.get(s);return l.get(i).length-r.length});t.value=[...l].flatMap(([,s])=>s).sort((s,i)=>u.indexOf(s.parentPageTitle)-u.indexOf(i.parentPageTitle)||s.point-i.point)}}function*N(e,t){const a=k(e.title,t);if(a){yield{path:e.path,parentPageTitle:y(e),title:e.title,display:a,page:e,content:null,point:1};return}for(const o of e.contents){const c=k(o.header,t);if(c){yield{path:e.path+(o.slug?`#${o.slug}`:""),parentPageTitle:y(e),title:e.title,display:c,page:e,content:null,point:2};continue}const l=k(o.content,t);l&&(yield{path:e.path+(o.slug?`#${o.slug}`:""),parentPageTitle:y(e),title:e.title,display:[{type:"header",str:`${o.header}
+`},...l],page:e,content:null,point:10})}}function y(e){const t=e.path.split("/");let a="/";return t[1]&&(a=`/${t[1]}/`),(E.value.get(a)||e).title}function k(e,t){const a=[];let o=0;const c=e.toLowerCase().replace(/\s/gu," ");let l=0,n=c.indexOf(t,l);if(n<0)return null;for(;n>=0;){const s=n+t.length;if(u(e.slice(l,n),"normal"),u(e.slice(n,s),"highlight"),l=s,n=c.indexOf(t,l),o>100)break}return u(e.slice(l),"normal"),a.filter(s=>s.str);function u(s,i){let r=s;i==="normal"&&r.length>100&&o===0&&(r=`\u2026 ${r.slice(-10)}`);let d=!1;if(o+r.length>100){if(a.some(f=>f.type==="ellipsis"))return;r=r.slice(0,Math.max(100-o,1)),d=!0}a.push({type:i,str:r}),o+=r.length,d&&(a.push({type:"ellipsis",str:" \u2026"}),o+=2)}}const z=_({name:"SearchBox",setup(){const e=m(""),t=m(!1),a=m(-1),o=F(e),c=B(()=>e.value&&t.value&&o.value.length),l=C();function n(){if(!c.value)return;let d=a.value-1;d<0&&(d=o.value.length-1),s(d)}function u(){if(!c.value)return;let d=a.value+1;d>=o.value.length&&(d=0),s(d)}function s(d){a.value=d}function i(){a.value=-1}function r(d){if(!c.value)return;const f=o.value[d];f&&l.push(f.path)}return{query:e,focused:t,focusIndex:a,suggestions:o,activeSuggestion:c,onUp:n,onDown:u,focus:s,unfocus:i,go:r}}}),G={class:"search-box",role:"search"},H=["onMousedown","onMouseenter"],V=["href"],K={key:0,class:"parent-page-title"},O={class:"suggestion-row"},R={class:"page-title"},j={class:"suggestion-content"};function W(e,t,a,o,c,l){return h(),p("div",G,[$(g("input",{ref:"input","onUpdate:modelValue":t[0]||(t[0]=n=>e.query=n),"aria-label":"Search",class:b({focused:e.focused}),placeholder:"Search",autocomplete:"off",spellcheck:"false",onFocus:t[1]||(t[1]=()=>e.focused=!0),onBlur:t[2]||(t[2]=()=>e.focused=!1),onKeyup:[t[3]||(t[3]=w(n=>e.go(e.focusIndex),["enter"])),t[4]||(t[4]=w((...n)=>e.onUp&&e.onUp(...n),["up"])),t[5]||(t[5]=w((...n)=>e.onDown&&e.onDown(...n),["down"]))]},null,34),[[I,e.query]]),e.activeSuggestion?(h(),p("ul",{key:0,class:"suggestions",onMouseleave:t[7]||(t[7]=(...n)=>e.unfocus&&e.unfocus(...n))},[(h(!0),p(P,null,T(e.suggestions,(n,u)=>(h(),p("li",{key:u,class:b(["suggestion",{focused:u===e.focusIndex}]),onMousedown:s=>e.go(u),onMouseenter:s=>e.focus(u)},[g("a",{href:n.path,onClick:t[6]||(t[6]=U(()=>{},["prevent"]))},[n.parentPageTitle&&(!e.suggestions[u-1]||e.suggestions[u-1].parentPageTitle!==n.parentPageTitle)?(h(),p("div",K,v(n.parentPageTitle),1)):L("",!0),g("div",O,[g("div",R,v(n.title||n.path),1),g("div",j,[(h(!0),p(P,null,T(n.display,(s,i)=>(h(),p("span",{key:i,class:b(s.type)},v(s.str),3))),128))])])],8,V)],42,H))),128))],32)):L("",!0)])}var J=x(z,[["render",W],["__scopeId","data-v-75931839"],["__file","SearchBox.vue"]]);export{J as default};
